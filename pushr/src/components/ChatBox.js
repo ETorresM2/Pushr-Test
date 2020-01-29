0e453b16b9132fd3bd2b736873be55a1b0bfc755
Edwin Torres <edwin.torres.jr2@gmail.com>
2020-01-29 18:09:15 -0500

    chatbox.js created

---
+++ pushr/src/components/ChatBox.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./ChatBox.css";
+export default ({ text, username, handleTextChange }) => (
+  <div>
+    <div className="row">
+      <div className="col-xs=12">
+        <div className="chat">
+          <div className="col-xs-5 col-xs-offest-3">
+            <input
+              type="text"
+              value={text}
+              placeholder="chat here"
+              className="form-control"
+              onChange={handleTextChange}
+              onKeyDown={handleTextChange}
+            />
+          </div>
+          <div className="clearfix"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
