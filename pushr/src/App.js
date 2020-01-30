c0e367d9b60d2533f615075fe3751e66ce7163f7
Edwin Torres <edwin.torres.jr2@gmail.com>
2020-01-30 17:29:06 -0500

    app.js

--- pushr/src/App.js
+++ pushr/src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import axios from "axios"
+import Pusher from "Pusher-js"
+import ChatList from "./components/ChatList"
+import ChatBox from "./components/ChatBox"
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "",
+      username: "",
+      chats: []
+    }
+  }
+  componentDidMount(){
+    const username=window.prompt("Username: ", "Anonymous")
+    this.setState({username})
+    const pusher= new Pusher('APP_KEY', {
+      cluster: 'APP_CLuster',
+      encrypted: true
+    });
+    const channel = pusher.subscribe("chat")
+    channel.bind("message", data => {
+      this.setState({chats: [...this.state.chats, data], test: ''})
+    })
+    this.handleTextChange=this.handleTextChange.bind(this)
+  }
+  handleTextChange(e) {
+    if(e.keyCode === 13) {
+      const payload = {
+        username: this.state.username,
+        message: this.state.text
+      };
+      axios.post("http://localhost:5000/message", payload);
+    } else {
+      this.setState({text: e.target.value})
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React-Pusher Chat</h1>
+        </header>
+        <section>
+          <ChatList chats={this.state.chats} />
+          <ChatBox
+            text={this.state.text}
+            username={this.state.username}
+            handleTextChange={this.handleTextChange}
+          />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
